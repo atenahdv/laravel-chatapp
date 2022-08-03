25153167d652e62b8a66bf896fcc53f62c4eba9f
atenahadavi <atenahadavi@gmail.com>
2022-08-03 23:27:47 +0430

    add messages function in ChatRoom model

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ChatRoom extends Model
 {
     use HasFactory;
+
+    public function messages(){
+       return $this->hasMany(ChatMessage::class);
+    }
 }

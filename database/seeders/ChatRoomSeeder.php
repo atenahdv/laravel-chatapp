8e0e5a4fe100cf4064c848e902c720bd7f0e1365
atenahadavi <atenahadavi@gmail.com>
2022-08-03 22:44:41 +0430

    ChatRoomSeeder

---
+++ database/seeders/ChatRoomSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ChatRoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('chat_rooms')->insert([
+            'name' => 'General'
+        ] 
+        );
+        DB::table('chat_rooms')->insert([
+            'name' => 'Tech Talk'
+        ] 
+        );
+    }
+}

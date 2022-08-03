a5f5dcbfd5e586eaa808593b7ee690480c79534c
atenahadavi <atenahadavi@gmail.com>
2022-08-03 23:26:49 +0430

    create ChatRoomController

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatMessage;
+use App\Models\ChatRoom;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request){
+        return ChatRoom::all();
+    }
+    public function messages(Request $request , $roomId){
+        return ChatMessage::where('chat_room_id' , $roomId)
+        ->with('user')
+        ->orderBy('created_at','DESC')
+        ->get();
+
+    }
+
+    public function newMessage(Request $request , $roomId){
+        $newMessage = new ChatMessage;
+        $newMessage->user_id = Auth::id();
+        $newMessage->chat_room_id = $roomId;
+        $newMessage->message = $request->message;
+        $newMessage->save();
+
+        return $newMessage;
+    }
+}

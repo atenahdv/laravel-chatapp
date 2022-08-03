704bffd07c8ca83c2c22319aecc09411a4b9e1bb
atenahadavi <atenahadavi@gmail.com>
2022-08-03 23:27:18 +0430

    add room and user function in chatMessage model

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ChatMessage extends Model
 {
     use HasFactory;
+
+    public function room(){
+        return $this->hasOne(ChatRoom::class, 'id', 'chat_room_id');
+    }
+
+    public function user(){
+        return $this->hasOne(user::class, 'id', 'user_id');
+    }
 }

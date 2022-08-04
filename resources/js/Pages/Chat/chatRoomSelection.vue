9bcecf7b9fab6265bccdbbe16ae15968f0f0e0a1
atenahadavi <atenahadavi@gmail.com>
2022-08-04 19:23:55 +0430

    create chatroom selection

--- resources/js/Pages/Chat/chatRoomSelection.vue
+++ resources/js/Pages/Chat/chatRoomSelection.vue
@@ -1,10 +1,38 @@
 <template>
-<div>Selection View</div>
+            <div class="grid grid-cols-2">
+            <div class="font-bold text-xl">
+
+              {{ selected.name }} Chat
+            </div>
+            <div>
+              <select
+              v-model="selected"
+              @change="$emit('roomchanged' , selected)"
+              class="float-right">
+              
+              <option 
+              v-for="(room , index) in rooms "
+              :key="index"
+              :value="room">
+              {{ room.name }}
+              </option>
+              </select>
+
+            </div>
+            </div>
 
 </template>
 
 <script>
 export default{
-
+props : ['rooms', 'currentRoom'],
+data : function(){
+  return {
+    selected : ''
+  }
+},
+created(){
+  this.selected = this.currentRoom;
+}
 }
 </script>

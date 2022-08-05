e9b68d8aa066f4dc79d139ae4bd2707ce82bf851
atenahadavi <atenahadavi@gmail.com>
2022-08-05 13:46:44 +0430

    add broadcast rout

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Broadcast Channels
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+Broadcast::channel('chat.{roomId}', function ($user, $roomId) {
+    if(Auth::check()){
+        return ['id' => $user->id , 'name' => $user->name];
+    }
 });
